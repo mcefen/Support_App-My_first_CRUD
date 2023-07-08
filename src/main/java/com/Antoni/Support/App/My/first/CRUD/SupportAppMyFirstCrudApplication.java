4547a0fae64ac2b26475e0a43af6ba27e572663d
antonio <viz7mor@gmail.com>
2023-07-08 17:47:25 +0200

    primer commit

---
+++ src/main/java/com/Antoni/Support/App/My/first/CRUD/SupportAppMyFirstCrudApplication.java
@@ -0,0 +1,13 @@
+package com.Antoni.Support.App.My.first.CRUD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupportAppMyFirstCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupportAppMyFirstCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Antoni/Support/App/My/first/CRUD/SupportAppMyFirstCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Antoni.Support.App.My.first.CRUD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupportAppMyFirstCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
